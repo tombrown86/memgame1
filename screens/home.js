c3490f92cde512c5602ee5889b9cd22bd3eb068c
tombrown86 <tombrown86@gmail.com>
2023-09-19 09:48:53 +0100

    second commit

--- screens/home.js
+++ screens/home.js
@@ -11,9 +11,9 @@ const Home = () => {
   return (
     <View style={styles.homeouter}>
         <View style={styles.homeinner}>
-            <Text style={styles.welcometext}>Welcome</Text>
-            <Text style={styles.bannertext}>Improve your MEMORY skills</Text> 
-            <Text style={styles.smallbannertext}>by playing randomly chosen mini games & tests</Text>
+            <Text style={styles.welcometext}>Welcome!</Text>
+            <Text style={styles.bannertext}>Improve your MEMORY skills&#8230;</Text> 
+            <Text style={styles.smallbannertext}>&#8230;by playing randomly chosen mini brain games!&#8230;</Text>
         </View>
         <View style={styles.homeinner}>
             <Image  style={{ width: '100%', height: 200 }} source={require('../images/chillbraincopy.png')} />
@@ -31,27 +31,6 @@ const Home = () => {
 export default Home
 
 const styles = StyleSheet.create({
-    welcometext: {
-        fontSize: 20,
-        textAlign: 'center'
-    },
-    bannertext: {
-        marginTop: 20,
-        fontSize: 18,
-        textAlign: 'center'
-    },
-    smallbannertext: {
-        marginTop: 10,
-        fontSize: 15,
-        textAlign: 'center'
-    },
-    recommendtext: {
-        marginTop: 14,
-        marginLeft: 10,
-        marginRight: 10,
-        fontSize: 12,
-        textAlign: 'center'
-    },
     homeouter: {
         flex: 1
     },
@@ -60,5 +39,32 @@ const styles = StyleSheet.create({
         flex: 1,
         height: '100%',
         justifyContent: 'center'
+    },
+    welcometext: {
+        fontSize: 20,
+        textAlign: 'center',
+        fontFamily: "karla"
+    },
+    bannertext: {
+        marginTop: 20,
+        fontSize: 18,
+        textAlign: 'center',
+        fontFamily: "rubik",
+        fontWeight: 'bold'
+    },
+    smallbannertext: {
+        marginTop: 12,
+        fontSize: 15,
+        fontFamily: "rubik",
+        textAlign: 'center'
+    },
+    recommendtext: {
+        marginTop: 14,
+        marginLeft: 10,
+        marginRight: 10,
+        fontSize: 12,
+        textAlign: 'center',
+        fontFamily: "karla",
+        fontStyle: 'italic'
     }
 })
